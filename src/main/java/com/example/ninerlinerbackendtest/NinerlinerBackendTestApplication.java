18150eb38bcc4dead25640b50c1e265f801cd19e
vitaliysolomonov <454115@gmail.com>
2022-05-20 14:02:48 -0500

    1st

---
+++ src/main/java/com/example/ninerlinerbackendtest/NinerlinerBackendTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ninerlinerbackendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinerlinerBackendTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinerlinerBackendTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ninerlinerbackendtest/NinerlinerBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ninerlinerbackendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinerlinerBackendTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
